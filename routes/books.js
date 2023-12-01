0ca14571c901e5724b90f9fade4e8259dba153cd
Ehtisham Ahmed Gondal <ehtishamahmedgondal@gmail.com>
2023-12-01 09:55:03 +0500

    added stripe payment

--- routes/books.js
+++ routes/books.js
@@ -7,8 +7,9 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-//.end point to fetch the rooms
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
+//.end point to fetch the rooms
 router.get('/fetchBooks', [], async (req, res) => {
 
   try {
@@ -121,7 +122,46 @@ router.delete('/deleteBook/:id', async (req, res) => {
   }
 });
 
+router.post('/payment/:id', async (req, res) => {
+  const bookId = req.params.id;
 
+  try {
+    const bookDetails = await Book.findOne({ _id: bookId });
+
+    if (!bookDetails) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map(item => {
+        if (!bookDetails.price) {
+          console.error(`Invalid or missing price for book ID ${bookId}`);
+          throw new Error('Invalid or missing price for book');
+        }
+        const unitAmountInCents = bookDetails.price * 100;
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: bookDetails.name,
+            },
+            unit_amount: Math.round(unitAmountInCents), // Round to the nearest whole number
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.CLIENT_URL}/success.html`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+    });
+
+    res.json({ url: session.url });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 module.exports = router;
