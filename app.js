1e29c66982477469da810cad4c7f9dd4a8a5367b
Ehtisham Ahmed Gondal <ehtishamahmedgondal@gmail.com>
2023-12-03 21:21:23 +0500

    addded notification system using socket.io

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-const express  = require('express');
-const mongoose = require('./db')
-var cors = require('cors');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('./db');
+const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 require('dotenv').config();
-const app  = express();
+
+const app = express();
+const server = http.createServer(app); // Pass the app instance to createServer
+const io = socketIo(server);
+
+
+
 mongoose();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-
-    res.send('shami HMS').statusCode(200)
-}); 
+app.get('/', (req, res) => {
+  res.send('shami HMS').status(200);
+});
 
 
-app.use('/api/books/', require('./routes/books'))
-app.use('/api/auth/', require('./routes/authentication'))
+io.on('connection', (socket) => {
+  console.log('User connected');
 
-app.listen(port, ()=>{
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 
-    console.log(`App listening at the port http://localhost:${port}`)
-})
+  socket.on('new_like', (data) => {
+    io.emit('new_like', { message: data.message });
+  });
+});
+
+
+app.use('/api/books/', require('./routes/books'));
+
+server.listen(port, () => {
+  console.log(`App listening at the port http://localhost:${port}`);
+});
